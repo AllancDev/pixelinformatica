fd3ccc390307d0d53750b8d351190952a4a7ce43
Allan Camargo <allan.cdev@gmail.com>
2019-09-20 15:25:53 -0300

    Alteração Fonte

--- js/script.js
+++ js/script.js
@@ -205,5 +205,17 @@
 		})(jQuery);
 	});
 
+	$(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('a[href="#top"]').fadeIn();
+        } else {
+            $('a[href="#top"]').fadeOut();
+        }
+    });
+
+    $('a[href="#top"]').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 
 })(window.jQuery);
